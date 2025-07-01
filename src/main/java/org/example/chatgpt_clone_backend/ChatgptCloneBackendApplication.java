611245aaa173aa7740564070634c35f1774c657f
kimjihun <xxxjjhhh@naver.com>
2025-07-01 23:31:24 +0900

    * init

---
+++ src/main/java/org/example/chatgpt_clone_backend/ChatgptCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.chatgpt_clone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptCloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptCloneBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/chatgpt_clone_backend/ChatgptCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chatgpt_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptCloneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
