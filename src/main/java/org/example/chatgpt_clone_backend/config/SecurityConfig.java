a8465d3b65951cdff5e74838a852caa2dd08bf20
kimjihun <xxxjjhhh@naver.com>
2025-07-01 23:54:06 +0900

    * 스프링 시큐리티 SecurityConfig 클래스 추가 완료 : PasswordEncoder, SecurityFilterChain 등록 ref #2

---
+++ src/main/java/org/example/chatgpt_clone_backend/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package org.example.chatgpt_clone_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 비밀번호 단방향(BCrypt) 암호화용 Bean
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // SecurityFilterChain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // CSRF 보안 필터 disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        // 기본 Form 기반 인증 필터들 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        // 기본 Basic 인증 필터 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        // 인가
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**").permitAll());
+
+        // 세션 필터 설정 (STATELESS)
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+
+}
