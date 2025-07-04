1d7fd790da592621df02cfa6492f2025b1dc46a7
kimjihun <xxxjjhhh@naver.com>
2025-07-04 19:50:51 +0900

    * Jwt 도메인 : Refresh 토큰 저장소 삭제 스케줄 등록 close #41

--- src/main/java/org/example/chatgpt_clone_backend/ChatgptCloneBackendApplication.java
+++ src/main/java/org/example/chatgpt_clone_backend/ChatgptCloneBackendApplication.java
@@ -3,9 +3,11 @@ package org.example.chatgpt_clone_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class ChatgptCloneBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/example/chatgpt_clone_backend/domain/jwt/repository/RefreshRepository.java
+++ src/main/java/org/example/chatgpt_clone_backend/domain/jwt/repository/RefreshRepository.java
@@ -4,10 +4,17 @@ import org.example.chatgpt_clone_backend.domain.jwt.entity.RefreshEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 public interface RefreshRepository extends JpaRepository<RefreshEntity, Long> {
 
     Boolean existsByRefresh(String refreshToken);
 
     @Transactional
     void deleteByRefresh(String refresh);
+
+    // 특정일 지난 refresh 토큰 삭제
+    @Transactional
+    void deleteByCreatedDateBefore(LocalDateTime createdDate);
+
 }
