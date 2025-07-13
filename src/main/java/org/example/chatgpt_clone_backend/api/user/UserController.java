e581369cb15649db9d8abf074ac30584e8dbf191
kimjihun <xxxjjhhh@naver.com>
2025-07-14 08:33:41 +0900

    * User 도메인 : 유저 정보 조회 close #74

--- src/main/java/org/example/chatgpt_clone_backend/api/user/UserController.java
+++ src/main/java/org/example/chatgpt_clone_backend/api/user/UserController.java
@@ -6,10 +6,7 @@ import org.example.chatgpt_clone_backend.domain.user.service.UserService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
@@ -38,7 +35,7 @@ public class UserController {
     }
 
     // 유저 정보
-    @GetMapping("/user/me")
+    @GetMapping("/user")
     public UserResponseDTO userMeApi() {
         return userService.readUser();
     }
