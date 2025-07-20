1f89203b6138682d47404e5813212ad848101a5d
kimjihun <xxxjjhhh@naver.com>
2025-07-21 03:11:00 +0900

    * JpaAuditing Config 분리 close #86

--- src/main/java/org/example/chatgpt_clone_backend/ChatgptCloneBackendApplication.java
+++ src/main/java/org/example/chatgpt_clone_backend/ChatgptCloneBackendApplication.java
@@ -2,11 +2,9 @@ package org.example.chatgpt_clone_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableScheduling
 public class ChatgptCloneBackendApplication {
 
